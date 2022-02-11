bdf316fd356c807871ce02deb1f56e4633b147b7
prajwal <prajwal.pant9@gmail.com>
2022-02-12 01:10:50 +0545

    few new things

--- rust_test.rs
+++ rust_test.rs
@@ -1,4 +1,14 @@
 fn main(){
-    print!("prajwal\npant\nnepal\n");
+    let a=5;
+    let b=5.0;
+
+    println!("prajwal,pant,nepal");
+    println!("prajwal,pant,nepalpp");
+    println!("{}",a);
+    println!("the sume is : {}",b+2.1);
+    println!("this is\n\
+    first\n\
+    second\n\
+    third");//new line
 }
 
